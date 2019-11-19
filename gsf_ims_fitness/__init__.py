40dc41f710c471ae8602117304625a5f217827ec
Ross <djross@nist.gov>
2019-11-19 12:24:31 -0500

    Start new python project/repository

---
+++ gsf_ims_fitness/__init__.py
@@ -0,0 +1,10 @@
+"""
+`gsf_ims_fitness`: Python package for analyzing fitness data for GSF IMS project.
+
+"""
+
+# Versions should comply with PEP440.  For a discussion on single-sourcing
+# the version across setup.py and the project code, see
+# https://packaging.python.org/en/latest/single_source_version.html
+__version__ = '0.1'
+
---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tues Nov 19 2019
+
+@author: djross
+"""
+
+from setuptools import setup, find_packages
+
+setup(name='gsf_ims_fitness',
+      version='0.1',
+      description='Python package for analyzing fitness data for GSF IMS project',
+      packages=find_packages(),
+      zip_safe=False)
