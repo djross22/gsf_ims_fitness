f38f5cffbabc0b48fbd19fd535356d495c5688f9
Ross <djross@nist.gov>
2019-11-27 13:36:19 -0500

    Chagne bound on fit parameters in fit_fitness_difference_curves()

--- gsf_ims_fitness/BarSeqFitnessFrame.py
+++ gsf_ims_fitness/BarSeqFitnessFrame.py
@@ -466,7 +466,7 @@ class BarSeqFitnessFrame:
             valid = ~(np.isnan(y) | np.isnan(s))
             
             p0 = [100, 1500, 200, 1.5]
-            bounds = [[0, 0, 1, 0.1], [2000, 5000, max(x), 5]]
+            bounds = [[0.1, 0.1, 0.1, 0.1], [2000, 5000, max(x), 5]]
             try:
                 popt, pcov = curve_fit(fit_fitness_difference_funct, x[valid], y[valid], sigma=s[valid], p0=p0, maxfev=len(x)*10000, bounds=bounds)
             except (RuntimeError, ValueError) as err:
